8a1abca30a1258f2371a0951f700fe4b483b1109
Olivier Laurent <olt@odoo.com>
2015-09-16 16:11:44 +0200

    [FIX] setup.py: make this 'setup.py' work
    remove reading the long description from file:
      when installing, it raises an error stating that it does not find
      that DESCRIPTION.rst file

--- setup.py
+++ setup.py
@@ -3,15 +3,27 @@ from setuptools import setup, find_packages
 from codecs import open
 from os import path
 
-here = path.abspath(path.dirname(__file__))
+long_description="""\
+A command line tool to upgrade your `Odoo <https://www.odoo.com>`_ database
+using the `Upgrade API <https://www.odoo.com/documentation/8.0/reference/upgrade_api.html>`_
+documented on the `official Odoo documentation <https://www.odoo.com/documentation>`_ (`v8.0 page
+<https://www.odoo.com/documentation/8.0/reference/upgrade_api.html>`_)
 
-# Get the long description from the relevant file
-with open(path.join(here, 'DESCRIPTION.rst'), encoding='utf-8') as f:
-    long_description = f.read()
+.. note:: You'll need an Odoo Enterprise Contract.
+
+It's the equivalent of filling the form on the `Upgrade platform <https://upgrade.odoo.com>`_ page.
+
+It allows to:
+
+* create a database upgrade request
+* upload a database dump
+* ask to process it
+* obtain the current status of your request
+"""
 
 setup(
     name='odoo-upgrade',
-    version='1.0',
+    version='1.0.3',
     description='Command line tool to upgrade your Odoo database',
     long_description=long_description,
     url='https://github.com/olilau/odoo-upgrade',
@@ -26,12 +38,16 @@ setup(
         'Environment :: Console',
         'Topic :: Database',
         'Programming Language :: Python :: 2.7',
+        'Operating System :: POSIX',
     ],
     keywords='odoo openerp database upgrade',
     packages=None,
     install_requires=['pycurl', 'pytz'],
     scripts = [
         'odoo-upgrade.py'
-    ]
+    ],
+    package_data = {
+        '': ['*.rst'],
+    },
 )
 
